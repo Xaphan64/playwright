0383479ba8e1edc6dcc03f7b9eda37c1a305bc3d
Xaphan64 <alex_daniel64@yahoo.com>
2026-07-11 10:08:06 +0300

    added login page automation

---
+++ tests/project-management/Login.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require("@playwright/test");
+
+// declare global
+let usernameField;
+let emailField;
+let passField;
+let confirmPassField;
+let loginBtn;
+let redirectBtn;
